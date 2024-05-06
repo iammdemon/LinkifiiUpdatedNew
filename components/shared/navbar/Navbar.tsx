d41ed0bb82e4aa7ae01bf5acaf1634ee9b7bd223
MD Emon <user@MacBook-Air.local>
2024-05-07 00:30:55 +0600

    first commit

---
+++ components/shared/navbar/Navbar.tsx
@@ -0,0 +1,42 @@
+import { SignedIn, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import Theme from "./Theme";
+import MobileNav from "./MobileNav";
+import GlobalSearch from "../search/GlobalSearch";
+
+const Navbar = () => {
+  return (
+    <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 sm:px-12 dark:shadow-none">
+      <Link href="/" className="flex items-center gap-1">
+        <Image
+          src="/assets/images/site-logo.svg"
+          width={73}
+          height={23}
+          alt="Linkifii"
+        />
+      </Link>
+      <GlobalSearch />
+      <div className="flex-between gap-5">
+        <Theme />
+        <SignedIn>
+          <UserButton
+            afterSignOutUrl="/sign-in"
+            appearance={{
+              elements: {
+                avatarBox: "h-10 w-10",
+              },
+              variables: {
+                colorPrimary: "#ff7000",
+              },
+            }}
+          />
+        </SignedIn>
+        <MobileNav />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
